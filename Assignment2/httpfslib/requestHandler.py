3c9823ec566be9238167a5ceaa6ca119aca38300
Eric-Z0 <ericzhuang603@gmail.com>
2017-11-05 19:51:07 -0500

    Content-type feature

--- Assignment2/httpfslib/requestHandler.py
+++ Assignment2/httpfslib/requestHandler.py
@@ -24,6 +24,12 @@ class requestHandler:
         self.__filePath = match.group(2)
         self.__path = path
 
+        cd_startpos = args.find("Content-Disposition")
+        if (cd_startpos >=0):
+            cd_message = args[cd_startpos:]
+            cd_endpos = cd_message.find("\r\n")
+            cd_message = cd_message[:cd_endpos]
+        
 
         if(self.__method == "GET"):
             self.__response = self.getFile(self.__filePath)
@@ -39,7 +45,7 @@ class requestHandler:
 
         fullFilePath = os.path.realpath(self.__path + filePath)
 
-        if (len(fullFilePath) < len(self.__path)):
+        if (not fullFilePath.startswith(self.__path)):
             response.setStatus(401, "Unauthorized")
         else:
             if (os.path.isdir(fullFilePath)):
@@ -49,6 +55,9 @@ class requestHandler:
                 if (not os.path.isfile(fullFilePath)):
                     response.setStatus(404, "File not found")
                 else:
+                    content = fullFilePath,split(".")[-1]
+                    contentType = analyzeContentType(content)
+                    response.addHeader("Content-Type", contentType)
                     with open(fullFilePath, 'r') as file:
                         fcontent = file.read()
                         response.setBody(fcontent)
@@ -74,3 +83,18 @@ class requestHandler:
 
     def getResponse(self):
         return self.__response
+
+    def analyzeContentType(content):
+        if (content == "txt" or "html"):
+            return "text/html; charset=utf-8"
+        if (content == "jpeg" or "gif" or "png" or "ico"):
+            return "image/" + content
+        if (content == "mp3" or "acc" or "midi"):
+            return "audio/" + content
+        if (content == "mp4" or "mpeg" or "avi"):
+            return "video/" + content
+        if (content == "pdf" or "rar" or "doc" or "jar" or "js" or "json"):
+            return "application/" + content
+            
+        
+        
